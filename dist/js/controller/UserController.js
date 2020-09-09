6ab551432b9fabe69bc06aa480c6bdffbefd96bf
cdsnascimento <69284317+cdsnascimento@users.noreply.github.com>
2020-09-08 23:19:40 -0300

    aula 54. G26
    Finalizando a aula 54, onde inserimos o método de exclusão

--- dist/js/controller/UserController.js
+++ dist/js/controller/UserController.js
@@ -87,7 +87,7 @@ class UserController{
                     <td>${Utils.dateFormat(data._register)}</td>
                     <td>
                         <button type="button" class="btn btn-edit btn-primary btn-xs btn-flat">Editar</button>
-                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                        <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                     </td>
                 `;
 
@@ -245,6 +245,17 @@ class UserController{
     }
 
     addEventsTr(tr){
+
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+            if(confirm("Deseja relamente excluir?")){
+
+                tr.remove();
+                this.updateCount();
+
+            }
+
+        });
 
         tr.querySelector('.btn-edit').addEventListener("click", e=>{
 
